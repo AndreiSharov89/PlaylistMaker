7a2a441a261cbb12da32824fd24cecf34ff6469f
Andrei <sharov89@gmail.com>
2025-10-28 12:20:27 +0300

    sprint 16 moving to clean and mvvm, review fix

--- app/src/main/java/com/example/playlistmaker/settings/ui/SettingsViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/SettingsViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker.settings.ui
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.playlistmaker.creator.Creator
@@ -42,6 +44,8 @@ class SettingsViewModel(
     companion object {
         fun getViewModelFactory(): ViewModelProvider.Factory = viewModelFactory {
             initializer {
+                val application = this[APPLICATION_KEY] as Application
+                Creator.initApplication(application)
                 SettingsViewModel(
                     sharingInteractor = Creator.provideSharingInteractor(),
                     settingsInteractor = Creator.provideSettingsInteractor()
