09faa7826eb64759d64f95588123ff3085b07552
Andrei Sharov <sharov89@gmail.com>
2026-01-16 21:18:11 +0300

    sprint 23 playlist layout

--- app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.playlistmaker.R
-import com.example.playlistmaker.databinding.FragmentPlaylistBinding
+import com.example.playlistmaker.databinding.FragmentPlaylistListBinding
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
 
-    private var _binding: FragmentPlaylistBinding? = null
+    private var _binding: FragmentPlaylistListBinding? = null
     private val binding get() = _binding!!
     private val viewModel: PlaylistsViewModel by viewModel()
     private var playlistAdapter: PlaylistAdapter? = null
@@ -25,7 +25,7 @@ class PlaylistsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
+        _binding = FragmentPlaylistListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
